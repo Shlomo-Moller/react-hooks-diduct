9e5bc31c2df68f60a901587db50423b6d2ce6689
Shlomo Moller <shlomo.moller@tandemg.com>
2022-03-14 15:53:05 +0200

    move examples

--- app/src/examples.js
+++
@@ -1,7 +0,0 @@
-import ThemeToggler from './components/ThemeToggler'
-
-const examples = [
-	{ id: 'ThemeToggler', element: <ThemeToggler /> },
-]
-
-export default examples
---
+++ app/src/helpers/examples.js
@@ -0,0 +1,9 @@
+import ThemeToggler from '../components/theme-context/ThemeToggler'
+import ThemeWithTogglerToggler from '../components/theme-and-toggler-context/ThemeWithTogglerToggler'
+
+const examples = [
+	{ id: 'ThemeToggler', element: <ThemeToggler /> },
+	{ id: 'ThemeWithTogglerToggler', element: <ThemeWithTogglerToggler /> },
+]
+
+export default examples
