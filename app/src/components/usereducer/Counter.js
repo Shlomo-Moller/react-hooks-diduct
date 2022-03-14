a5829a09689ede6bf2c17bc5204d2dbd480493d6
Shlomo Moller <shlomo.moller@tandemg.com>
2022-03-14 18:16:57 +0200

    Add the Counter code example

---
+++ app/src/components/usereducer/Counter.js
@@ -0,0 +1,15 @@
+import { useReducer } from 'react'
+import reducer from './reducer'
+
+function Counter() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 })
+  return (
+    <>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+    </>
+  )
+}
+
+export default Counter
--- app/src/helpers/examples.js
+++ app/src/helpers/examples.js
@@ -1,9 +1,11 @@
 import ThemeToggler from '../components/theme-context/ThemeToggler'
 import ThemeWithTogglerToggler from '../components/theme-and-toggler-context/ThemeWithTogglerToggler'
+import Counter from '../components/usereducer/Counter'
 
 const examples = [
 	{ id: 'ThemeToggler', element: <ThemeToggler /> },
 	{ id: 'ThemeWithTogglerToggler', element: <ThemeWithTogglerToggler /> },
+	{ id: 'Counter', element: <Counter /> },
 ]
 
 export default examples
