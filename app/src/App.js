ed5d0aa04dbffe01bda0f6b58ddff1baeeb19389
Shlomo Moller <shlomo.moller@tandemg.com>
2022-03-14 11:27:03 +0200

    Add App.css, examples.js, & update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,31 @@
+import { useState } from 'react'
+import examples from './examples'
+import './App.css'
+
 const App = () => {
-  return "I'm App"
+
+  const [exampleIndex, setExampleIndex] = useState(-1)
+
+  const onSelect = e => setExampleIndex(e.target.value)
+
+  return (
+    <>
+      <h1>React Hooks Diduct</h1>
+
+      <select value={exampleIndex} onChange={onSelect}>
+        <option value={-1} className='option-placeholder'>Select...</option>
+        {examples.map((example, index) => (
+          <option value={index} key={example.id}>
+            {example.id}
+          </option>
+        ))}
+      </select>
+      
+      <div className='example-wrapper'>
+				{exampleIndex >= 0 ? examples[exampleIndex].element : ''}
+			</div>
+    </>
+  )
 }
 
 export default App
---
+++ app/src/examples.js
@@ -0,0 +1,7 @@
+import ThemeToggler from './components/ThemeToggler'
+
+const examples = [
+	{ id: 'ThemeToggler', element: <ThemeToggler /> },
+]
+
+export default examples
